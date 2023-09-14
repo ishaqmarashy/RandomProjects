a417df306e6f95f12aad382ce7199ce1a99dcfe2
ishaqmarashy <34040118+ishaqmarashy@users.noreply.github.com>
2023-09-14 14:58:52 +0400

    Code up to lighing

---
+++ Cpp/learnOpenGl/learnOpenGl/7.2.camera.fs
@@ -0,0 +1,14 @@
+#version 330 core
+#extension GL_ARB_separate_shader_objects : enable
+
+out vec4 FragColor;
+in vec4 Color;
+  
+in vec2 TexCoord;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main()
+{
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(1.0 - TexCoord.x, TexCoord.y)), 0.2)*Color;
+}
